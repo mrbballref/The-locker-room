d36de7728158796342c24f8b10dc2a6acc11e21a
mrbballref <42037448+mrbballref@users.noreply.github.com>
2026-05-29 06:15:56 -0500

    Add shared types package

---
+++ packages/types/index.ts
@@ -0,0 +1,7 @@
+export type UserRole = 'super_admin' | 'administrator' | 'assignor' | 'official' | 'observer' | 'coach';
+
+export interface FilmVideo {
+ id:string;
+ title:string;
+ storagePath:string;
+}
